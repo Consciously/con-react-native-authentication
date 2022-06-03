003a2d8ae5e18251f2cba327a9b35ae966ba3bc7
Stefan Ihle <stefanihle@gmx.net>
2022-06-03 15:41:30 +0200

    creating new users

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyDUA96C-Ns0TZpESsBqOXc6dQX9s3vm6ws';
 
-const createUser = async (email, password) => {
+export const createUser = async (email, password) => {
 	const response = await axios.post(
 		`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
 		{
