bc2e2621ca8ddb588f591a63604e0540a84e3a79
Stefan Ihle <stefanihle@gmx.net>
2022-06-07 12:07:52 +0200

    extracting authentication token

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
 import { createUser } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { Alert } from 'react-native';
@@ -7,10 +7,13 @@ import { Alert } from 'react-native';
 function SignupScreen() {
 	const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+	const authCtx = useContext(AuthContext);
+
 	const signupHandler = async ({ email, password }) => {
 		setIsAuthenticating(true);
 		try {
-			await createUser(email, password);
+			const token = await createUser(email, password);
+			authCtx.authenticate(token);
 		} catch (error) {
 			Alert.alert(
 				'Authentication failed!',
